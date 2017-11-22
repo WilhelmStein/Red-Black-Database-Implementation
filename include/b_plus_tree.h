212220bad52a6706508ed8aba843d8df7807a9e5
WilhelmStein <johnfreeman16561@gmail.com>
2017-11-22 17:58:28 +0200

    BP_init(), Bp_delete() start

--- include/b_plus_tree.h
+++ include/b_plus_tree.h
@@ -1,8 +1,15 @@
 #ifndef B_PLUS_TREE_H
 #define B_PLUS_TREE_H
 
-struct b_plus_tree{
+#define B_PLUS_CHILDREN_NUM 3
+
+struct bPlusNode {
+    struct bPlusNode *children[B_PLUS_CHILDREN_NUM];
 
 };
 
+typedef struct bPlusNode * bPlusTree;
+
+int BP_init(bPlusTree *ptr);
+int BP_delete(bPlusTree *ptr);
 #endif
