371fc3fd84e0b3ab743c2be139c2c9dc3d36a77c
Brewknight <jmaliaras@gmail.com>
2017-11-29 14:18:33 +0200

    started insert

--- src/AM.c
+++ src/AM.c
@@ -13,23 +13,35 @@ scanData scanTable[MAXSCANS];
 #define BLACK ('B')
 #define RED   ('R')
 
-#define DATA        (1)  //arbitrary
+//RED BLOCKS
+//      IDENTIFIER   (0)  //char
+#define RECORDS      (1)  //int
+#define KEY          (5)  //arbitrary
+#define VALUE        (5 + (int)metaData[ATTRLENGTH1]) //arbitrary
 
+//BLACK BLOCKS
+//      IDENTIFIER   (0) //char
+#define NUMKEYS      (1) //int
+#define FIRST        (5) //int
+#define KEY          (9) //arbitrary
+#define POINTER      (5 + (int)metaData[ATTRLENGTH1]) //int
+
+//META BLOCK
 #define IDENTIFIER  (0)  //char
 #define ATTRTYPE1	  (1)  //char	
 #define ATTRLENGTH1 (2)  //int
 #define ATTRTYPE2	  (3)  //char
 #define ATTRLENGTH2 (4)  //int
 #define ROOT		    (5)  //int
-#define FILENAME	  (9) //char*
+#define FILENAME	  (9)  //char*
  
 
 #define CALL_OR_EXIT(call)		\
 {                           	\
 	BF_ErrorCode code = call; 	\
 	if(code != BF_OK) {       	\
-		BF_PrintError(code);    \
-		exit( AME_ERROR );		\
+		BF_PrintError(code);      \
+		exit( AME_ERROR );		    \
 	}                         	\
 }
 
@@ -96,6 +108,7 @@ int AM_CreateIndex(char *fileName,
 	printf("root at: %d\n", data[ROOT]);
 	printf("fileName = %s\n\n", &data[FILENAME]);
 
+  BF_Block_SetDirty(block);
 	CALL_OR_EXIT(BF_UnpinBlock(block));
 	CALL_OR_EXIT(BF_CloseFile(fileDesc));
 
@@ -211,26 +224,51 @@ int AM_InsertEntry(int fileDesc, void *value1, void *value2) {
 
 	char *metaData = BF_Block_GetData(metaBlock);
 
-	int root = ROOT;
+	int root = (int)metaData[ROOT];
+
   //First Insert//
 	if(root == 0)
 	{
-
-		BF_Block *newBlock;
-		BF_Block_Init(&newBlock);
-		CALL_OR_EXIT( BF_AllocateBlock(fileDesc, newBlock) );
-		char *data = BF_Block_GetData(newBlock);
+    //Create new red block
+		BF_Block *newRedBlock;
+		BF_Block_Init(&newRedBlock);
+		CALL_OR_EXIT( BF_AllocateBlock(fileDesc, newRedBlock) );
+    int redBlockCounter;
+    CALL_OR_EXIT( BF_GetBlockCounter(fileDesc, &redBlockCounter) );
+    CALL_OR_EXIT( BF_GetBlock(fileDesc, redBlockCounter, newRedBlock) );
+		char *data = BF_Block_GetData(newRedBlock);
 
 		data[IDENTIFIER] = RED;
 
-		int blockCount;
-		CALL_OR_EXIT( BF_GetBlockCounter(fileDesc, &blockCount) );
-    blockCount -= 1;  //Not sure about that!
+    memcpy( &data[KEY], value1, (size_t)metaData[ATTRLENGTH1] );
+    memcpy( &data[VALUE], value2, (size_t)metaData[ATTRLENGTH2] );
+    int records = 1;
+    memcpy( &data[RECORDS], &records , 4);
 
-		memcpy(&metaData[ROOT], &blockCount, 4);
 
-    
+    //Create new black block
+    BF_Block *newBlackBlock;
+    BF_Block_Init(&newBlackBlock);
+    CALL_OR_EXIT( BF_AllocateBlock(fileDesc, newBlackBlock) );
+    int blackBlockCounter;
+    CALL_OR_EXIT( BF_GetBlockCounter(fileDesc, &blackBlockCounter) );
+    CALL_OR_EXIT( BF_BF_GetBlock(fileDesc, blackBlockCounter, newBlackBlock) );
+    data = BF_Block_GetData(newBlackBlock);
+
+    data[IDENTIFIER] = BLACK;
+
+    memcpy( &metaData[ROOT], &blackBlockCounter, 4 );
+
+    memcpy( &data[KEY], value1, (size_t)metaData[ATTRLENGTH1] );
+    memcpy( &data[POINTER], &blackBlockCounter, 4 );
+
+    return (AM_errno = AME_OK);
 	}
+  else {
+    ;
+  }
+
+
 	return AME_OK;
 }
 
